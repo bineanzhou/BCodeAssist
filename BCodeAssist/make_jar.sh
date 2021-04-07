b46d1c642d2f5db27fa4bd446926fb9d85bd7c92
bineanzhou <bineanzhou@163.com>
2021-04-07 11:53:51 +0800

    修改字节码

--- BCodeAssist/make_jar.sh
+++ BCodeAssist/make_jar.sh
@@ -5,7 +5,7 @@ cd bcode_temp
 #jar -xvf ../bcodeassist/libs/ant-1.7.1.jar
 jar -xvf ../bcodeassist/libs/asm-4.0.jar
 jar -xvf ../bcodeassist/libs/asm-commons-4.0.jar
-jar -xvf ../bcodeassist/build/libs/bcodetool.jar
-mkdir -p ../jar
-jar -cvfM ../jar/bcode_tool.jar .
+jar -xvf ../bcodeassist/build/libs/bcodeassist.jar
+mkdir -p ../tools
+jar -cvfM ../tools/bcode_tool.jar .
 rm -rf bcode_temp
